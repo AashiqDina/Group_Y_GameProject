120c2fd4b38919e163ec28dd8cdf6603c5254983
Mohammad Bin Ali Soleja <31225174+MohammadSoleja@users.noreply.github.com>
2024-01-08 00:39:03 +0300

    Random Fixes

--- Assets/Scripts/HUD/StartScreenManager.cs
+++ Assets/Scripts/HUD/StartScreenManager.cs
@@ -1,49 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-/*public class StartOrRestartGame : MonoBehaviour
-{
-    public GameObject startScreenPanel; // Assign this in the Inspector
-    public InputAction startAction;
-    private bool isGameStarted = false;
-
-    private void OnEnable()
-    {
-        startAction.Enable();
-        startAction.performed += OnStartPerformed;
-    }
-
-    private void OnDisable()
-    {
-        startAction.Disable();
-        startAction.performed -= OnStartPerformed;
-    }
-
-    private void Start()
-    {
-        // Ensure the game starts with the start screen visible
-        startScreenPanel.SetActive(true);
-        Time.timeScale = 0f; // Pause the game
-    }
-
-    private void OnStartPerformed(InputAction.CallbackContext context)
-    {
-        if (!isGameStarted)
-        {
-            StartGame();
-        }
-    }
-
-    private void StartGame()
-    {
-        isGameStarted = true;
-        startScreenPanel.SetActive(false); // Hide the start screen
-        Time.timeScale = 1f; // Resume the game
-    }
-}
-
-using UnityEngine;
-using UnityEngine.InputSystem;*/
 using UnityEngine.UI;
 
 public class StartOrRestartGame : MonoBehaviour
